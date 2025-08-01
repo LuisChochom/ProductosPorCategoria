1f94838a98b50165f9802903b0e171259da362a6
Luis Chochom <Luis Chochom@DEVELOPER>
2025-08-01 13:02:33 -0600

    Agregar archivos de proyecto.

---
+++ ProductosPorCategoria/Default.aspx
@@ -0,0 +1,13 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="ProductosPorCategoria._Default" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+    <h3>En la siguiente lista se ven las categorias vendidas en el año 2019</h3>
+    <asp:DropDownList ID="ddlCategorias" runat="server" AutoPostBack="true" OnSelectedIndexChanged="ddlCategorias_SelectedIndexChanged">
+        <asp:ListItem Text="-- Selecciona una categoría --" Value="" />
+    </asp:DropDownList>
+
+    <br /><br />
+
+    <asp:GridView ID="gvProductos" runat="server" AutoGenerateColumns="true" />
+
+</asp:Content>
---
+++ ProductosPorCategoria/Default.aspx.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace ProductosPorCategoria
+{
+    public partial class _Default : Page
+    {
+        string connectionString = "Server=localhost\\SQLEXPRESS;Database=PracticaEval;Trusted_Connection=True;";
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                CargarCategoriasConVentas2019();
+            }
+        }
+
+        private void CargarCategoriasConVentas2019()
+        {
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                string query = @"
+                SELECT DISTINCT c.CodigoCategoria, c.Nombre
+                FROM Categoria c
+                JOIN Producto p ON c.CodigoCategoria = p.CodigoCategoria
+                JOIN Venta v ON p.CodigoProducto = v.CodigoProducto
+                WHERE YEAR(v.Fecha) = 2019";
+
+                SqlCommand cmd = new SqlCommand(query, conn);
+                conn.Open();
+                ddlCategorias.DataSource = cmd.ExecuteReader();
+                ddlCategorias.DataTextField = "Nombre";
+                ddlCategorias.DataValueField = "CodigoCategoria";
+                ddlCategorias.DataBind();
+            }
+
+            ddlCategorias.Items.Insert(0, new System.Web.UI.WebControls.ListItem("-- Selecciona una categoría --", ""));
+        }
+
+        protected void ddlCategorias_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string categoriaId = ddlCategorias.SelectedValue;
+
+            if (!string.IsNullOrEmpty(categoriaId))
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    string query = @"
+                    SELECT DISTINCT p.Nombre AS Producto
+                    FROM Producto p
+                    JOIN Venta v ON p.CodigoProducto = v.CodigoProducto
+                    WHERE p.CodigoCategoria = @categoriaId AND YEAR(v.Fecha) = 2019";
+
+                    SqlCommand cmd = new SqlCommand(query, conn);
+                    cmd.Parameters.AddWithValue("@categoriaId", categoriaId);
+                    conn.Open();
+                    gvProductos.DataSource = cmd.ExecuteReader();
+                    gvProductos.DataBind();
+                }
+            }
+            else
+            {
+                gvProductos.DataSource = null;
+                gvProductos.DataBind();
+            }
+        }
+    }
+}
